bfafe8b9f3f4a596065fda58eb81ea3bcbc78531
odilbekmatyokubov <97115091+odilbekmatyokubov@users.noreply.github.com>
2023-04-14 20:30:11 +0900

    Create 3Week.m

---
+++ 3Week.m
@@ -0,0 +1,16 @@
+%Exercise first
+radius=4;
+area=pi*radius^2;
+disp('area=');disp(area);
+circum=2*pi*radius;
+disp('circum=');disp(circum);
+
+
+%%%%%%%%%%%%%%%%%%%%
+
+%Exercise second
+t = 0:0.02:0.6;
+sin=10;%Variables
+y=3*sin*(pi*10*t);
+plot(t,y);
+title('Practise equation');
